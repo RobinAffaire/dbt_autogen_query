26c38783a8e3c8aec426ddde5af8b28d0f0b8765
RobinAffaire <122626565+RobinAffaire@users.noreply.github.com>
2024-10-24 12:21:33 +0200

    intermediate model + mart + sp - crerate raw tables

--- snowflake/sql_script.sql
+++ snowflake/sql_script.sql
@@ -3,3 +3,13 @@ use role accountadmin;
 create database if not exists flat_file;
 use database flat_file;
 create schema if not exists seed;
+
+
+create schema if not exists flat_file.base;
+create schema if not exists flat_file.staging;
+
+create database if not exists tech;
+create schema if not exists tech.intermediate;
+
+create database if not exists sap_p93;
+create schema if not exists sap_p93.raw;
